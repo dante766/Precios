7edd8e23335b7bdf8ebdb5b38f80e95fcdcc000e
dante766 <dantetolaba@gmail.com>
2025-07-31 14:52:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -720,18 +720,20 @@ function mostrarPreciosEMS() {
   agregarMensajeBot(html);
 }
 
-	function mostrarProveedores() {
-	  let html = "<strong>Lista de Proveedores:</strong><ul style='padding-left: 20px;'>";
+function mostrarProveedores() {
+  let html = "<strong>Lista de Proveedores:</strong><ul style='padding-left: 20px;'>";
 
-	  for (const nombre in proveedores) {
-		const contacto = proveedores[nombre]["CONTACTO"];
-		html += `
-		  <li style="margin-bottom: 10px;">
-			<i class="fas fa-user"></i> <u>${nombre}</u><br>
-			<i class="fab fa-whatsapp"></i> CONTACTO: <a href="${contacto}" target="_blank">WhatsApp</a>
-		  </li>
-		`;
-	  }  
+  for (const nombre in proveedores) {
+    const contacto = proveedores[nombre]["CONTACTO"];
+    html += `
+      <li style="margin-bottom: 10px;">
+        <i class="fas fa-user"></i> <u>${nombre}</u><br>
+        <i class="fab fa-whatsapp"></i> CONTACTO: <a href="${contacto}" target="_blank">WhatsApp</a>
+      </li>
+    `;
+  }
+
+} // ← esta llave te faltaba
 
 	// Muestra el resultado cuando se presiona el boton MEJORES PAGINAS
 	function mostrarMejoresPaginas() {
